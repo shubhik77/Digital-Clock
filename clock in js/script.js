3d2085bdd553f5000559df23584d69b5ca290dd7
shubhik77 <136617311+shubhik77@users.noreply.github.com>
2023-08-26 02:04:27 +0530

    Add files via upload

---
+++ clock in js/script.js
@@ -0,0 +1,18 @@
+function updateTime() {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    
+    const timeString = `${hours}:${minutes}:${seconds}`;
+    
+    const timeElement = document.getElementById('time');
+    timeElement.textContent = timeString;
+  }
+  
+  // Update time every second
+  setInterval(updateTime, 1000);
+  
+  // Initial call to set the time immediately
+  updateTime();
+  
